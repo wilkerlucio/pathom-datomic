b6a4ca292ca539fcc434192dcfd6d6c3235adec0
Rafael de F. Ferreira <rafael@rafaelferreira.net>
2020-02-27 19:20:23 -0300

    Load datomic on-prem dependency only when required

--- src/com/wsscode/pathom/connect/datomic/on_prem.clj
+++ src/com/wsscode/pathom/connect/datomic/on_prem.clj
@@ -1,6 +1,11 @@
-(ns com.wsscode.pathom.connect.datomic.on-prem
-  (:require [datomic.api :as d]))
+(ns com.wsscode.pathom.connect.datomic.on-prem)
+
+(defn- lazily [sym]
+  (let [f-thunk (delay (require (symbol (namespace sym)))
+                       (resolve sym))]
+    (fn [& args]
+      (apply @f-thunk args))))
 
 (def on-prem-config
-  {:com.wsscode.pathom.connect.datomic/datomic-driver-q  d/q
-   :com.wsscode.pathom.connect.datomic/datomic-driver-db d/db})
+  {:com.wsscode.pathom.connect.datomic/datomic-driver-q  (lazily 'datomic.api/q)
+   :com.wsscode.pathom.connect.datomic/datomic-driver-db (lazily 'datomic.api/db)})
